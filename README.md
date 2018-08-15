9f5282f1f5a295d5c82600064b8e1c7acdfb5bd2
Lei Shang <lei.shang@hotmail.com>
2018-08-15 19:57:19 +1000

    Added NLP

---
+++ README.md
@@ -0,0 +1,10 @@
+* Natural Language Processing
+
+
+* [Download Facebook Pre-trained word vectors](https://fasttext.cc/docs/en/english-vectors.html)
+
+Get only first 50 words with header
+```
+lshang@ubuntu:~/work.github/nlp$ cat wiki-news-300d-1M.vec | head -n 51 > vectors.vec
+
+```
---
+++ fastText/load_word_vectors.py
@@ -0,0 +1,37 @@
+import io
+import os
+
+"""
+https://fasttext.cc/docs/en/english-vectors.html
+
+"""
+
+
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    print("n = {},  d = {}".format(n, d))
+
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+
+    return data
+
+
+# get all
+# data = load_vectors('./data/wiki-news-300d-1M.vec')
+
+# get a short vector
+# lshang@ubuntu:~/work.github/nlp$ cat wiki-news-300d-1M.vec | head -n 51 > vectors.vec
+
+path = r"./fastText/data"
+path = os.path.abspath(path)
+file_name = 'words_short.vec'
+# file_name = 'wiki-news-300d-1M.vec'
+full_file_name = os.path.join(path, file_name)
+
+vector = load_vectors(full_file_name)
+
+print(vector)
